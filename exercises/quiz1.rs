70b963e55b7a68ea22308d7b116d1980e6017e98
Tunghohin <houhin0809@gmail.com>
2023-10-08 23:22:31 +0800

    finished quiz1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,13 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(x: i32) -> i32 {
+    match x {
+        0..=40 => x * 2,
+        _ => x,
+    }
+}
 
 // Don't modify this function!
 #[test]
