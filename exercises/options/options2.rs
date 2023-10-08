ea10bf6172b928254695046c263460ce551134bd
Tunghohin <houhin0809@gmail.com>
2023-10-09 02:54:18 +0800

    finished traits

--- exercises/options/options2.rs
+++ exercises/options/options2.rs
@@ -10,6 +10,7 @@ mod tests {
         let target = "rustlings";
         let optional_target = Some(target);
 
+        // TODO: Make this an if let statement whose value is "Some" type
         if let Some(word) = optional_target {
             assert_eq!(word, target);
         }
